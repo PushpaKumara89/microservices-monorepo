cce706560008c8a9017f65d0b330f0cbd69c7822
PushpaKumara89 <kpushpakumarajd@gmail.com>
2026-05-21 22:26:38 +0530

    Updated workflows to build and push to Docker Hub 15

--- order-service/src/main/java/pushk/order_service/controllers/OrderController.java
+++ order-service/src/main/java/pushk/order_service/controllers/OrderController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
@@ -11,5 +14,14 @@ public class OrderController {
     @GetMapping
     public String getOrders() {
         return "Order List from Order-Service v4";
+    }
+
+    @GetMapping("/host")
+    public String getHostName() {
+        try {
+            return "Request handled by Pod/Host: " + InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "Unable to determine Host Name: " + e.getMessage();
+        }
     }
 }
